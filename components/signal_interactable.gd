31288c7732c026f4d34cb3b8248ce5b2390f746d
nullptr448 <ghernandezlohaus.78@gmail.com>
2026-01-31 14:55:05 -0300

    Add `enabled` flag for interactables

--- components/signal_interactable.gd
+++ components/signal_interactable.gd
@@ -1,33 +1,30 @@
 class_name SignalInteractable
-extends Node
+extends Node2D
 
 signal interaction_available
-signal interaction_unavailable
 signal interaction_triggered
+signal interaction_unavailable
 
-@export var interaction_area: Area2D
+@export var enabled := true
+@onready var interaction_area: Area2D = $Area2D
 
 var interactor: Node2D = null
 
 func _ready() -> void:
-	interaction_area.body_entered.connect(_on_interaction_area_body_entered)
-	interaction_area.body_exited.connect(_on_interaction_area_body_exited)
+	interaction_area.body_entered.connect(_on_body_entered)
+	interaction_area.body_exited.connect(_on_body_exited)
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_accept") and interactor:
+	if event.is_action_pressed("interact") and interactor and enabled:
 		get_viewport().set_input_as_handled()
 		interaction_triggered.emit()
 
-func _on_interaction_area_body_entered(body: Node2D) -> void:
-	if not body.is_in_group("player"):
-		return
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player") or not enabled:
+		interactor = body
+		interaction_available.emit()
 
-	interactor = body
-	interaction_available.emit()
-
-func _on_interaction_area_body_exited(body: Node2D) -> void:
-	if not body.is_in_group("player"):
-		return
-
-	interactor = null
-	interaction_unavailable.emit()
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		interactor = null
+		interaction_unavailable.emit()

04c41d127cbd829ef331e6d829d009c1e8327f74
nullptr448 <ghernandezlohaus.78@gmail.com>
2026-02-05 16:20:35 -0300

    add new character animator component

---
+++ scripts/components/character_animator.gd
@@ -0,0 +1,41 @@
+class_name CharacterAnimator
+extends Node
+
+@export var animation_player: AnimationPlayer
+
+enum State {IDLE, MOVING}
+
+var current_state := State.IDLE
+
+func play_idle(direction: Vector2) -> void:
+	var animation_name := ""
+	
+	if abs(direction.x) > abs(direction.y):
+		animation_name = "idle_right" if direction.x > 0 else "idle_left"
+	else:
+		animation_name = "idle_down" if direction.y > 0 else "idle_up"
+		
+	if animation_player.current_animation != animation_name:
+		animation_player.play(animation_name)
+	
+	current_state = State.IDLE
+
+func play_moving(direction: Vector2) -> void:
+	var animation_name := ""
+	
+	if abs(direction.x) > abs(direction.y):
+		animation_name = "move_right" if direction.x > 0 else "move_left"
+	elif abs(direction.x) < abs(direction.y):
+		animation_name = "move_down" if direction.y > 0 else "move_up"
+	else:
+		# Lock current animation if already moving
+		if current_state == State.MOVING:
+			return
+		
+		# Default to horizontal
+		animation_name = "move_right" if direction.x > 0 else "move_left"
+
+	if animation_player.current_animation != animation_name:
+		animation_player.play(animation_name)
+	
+	current_state = State.MOVING

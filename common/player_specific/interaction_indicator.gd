5649008306c0cea5b8edbb30578306549b32d77c
nullptr448 <ghernandezlohaus.78@gmail.com>
2026-02-05 16:17:30 -0300

    remove various now unused scripts and other files

--- common/player_specific/interaction_indicator.gd
+++
@@ -1,8 +0,0 @@
-extends Sprite2D
-class_name InteractionIndicator
-
-func show_indicator():
-	visible = true
-
-func hide_indicator():
-	visible = false
--- common/simple_bot_input.gd
+++
@@ -1,9 +0,0 @@
-extends InputComponent
-class_name SimpleBotInput
-
-
-func get_accept_input() -> bool:
-	return true
-
-func get_cancel_input() -> bool:
-	return true

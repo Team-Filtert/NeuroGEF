aa54d32a4b07216538eddcc7d09c3676c73a0778
nullptr448 <ghernandezlohaus.78@gmail.com>
2026-02-05 18:41:41 -0300

    add reusable combatant scene for battles

---
+++ scripts/combat/combatant.gd
@@ -0,0 +1,25 @@
+class_name Combatant
+extends Node2D
+
+var combatant_name: String
+var max_health: float
+var health: float
+
+func _ready() -> void:
+	$HealthLabel.text = "HP: %s / %s" % [health, max_health]
+	$NameLabel.text = combatant_name
+	
+func take_damage(amount: float) -> void:
+	health -= amount
+	
+	if health <= 0:
+		health = 0
+		die()
+		
+	update_display()
+		
+func die() -> void:
+	queue_free()
+	
+func update_display() -> void:
+	$HealthLabel.text = "HP: %s / %s" % [health, max_health]

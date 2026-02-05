0e3c53cc6352a42626cc6cdbcb46baf493c2c808
nullptr448 <ghernandezlohaus.78@gmail.com>
2026-02-05 16:13:19 -0300

    rename signal interactable

--- common/camera_follower.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-class_name CameraFollower
-
-
-func _process(_delta):
-	global_position = get_viewport().get_camera_2d().global_position
--- scripts/components/interactable.gd
+++ scripts/components/interactable.gd
@@ -1,30 +1,27 @@
-class_name SignalInteractable
-extends Node2D
+class_name Interactable
+extends Area2D
 
-signal interaction_available
 signal interaction_triggered
-signal interaction_unavailable
 
 @export var enabled := true
-@onready var interaction_area: Area2D = $Area2D
 
 var interactor: Node2D = null
 
 func _ready() -> void:
-	interaction_area.body_entered.connect(_on_body_entered)
-	interaction_area.body_exited.connect(_on_body_exited)
+	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("interact") and interactor and enabled:
 		get_viewport().set_input_as_handled()
 		interaction_triggered.emit()
+		
+		print("DEBUG: Interaction triggered on entity %s" % get_parent().name)
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player") or not enabled:
+	if body.is_in_group("player") and enabled:
 		interactor = body
-		interaction_available.emit()
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		interactor = null
-		interaction_unavailable.emit()

d7266e4888d384d6fd37eeb443964e76891ef1fd
nullptr448 <ghernandezlohaus.78@gmail.com>
2026-02-07 20:29:42 -0300

    new party manager

---
+++ scripts/autoloads/party_manager.gd
@@ -0,0 +1,22 @@
+﻿extends Node
+
+var party: Array[CombatantData] = []
+
+func _ready() -> void:
+	var player_data: CombatantData = load("res://resources/combatants/player.tres")
+	var party_member_data: CombatantData = load("res://resources/combatants/party_member.tres")
+	
+	add_member(player_data)
+	
+	# Add the aditional 2 party members
+	add_member(party_member_data)
+	add_member(party_member_data)
+
+func add_member(member: CombatantData) -> void:
+	if party.size() >= 3:
+		return
+		
+	party.append(member)
+	
+func remove_member(member: CombatantData) -> void:
+	party.erase(member)

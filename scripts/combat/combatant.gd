15bfea3684ae75d9906f1a5220df486fa592c302
nullptr448 <ghernandezlohaus.78@gmail.com>
2026-02-07 20:29:15 -0300

    use custom resources for combatant data

--- scripts/combat/combatant.gd
+++ scripts/combat/combatant.gd
@@ -1,34 +1,29 @@
 class_name Combatant
 extends Node2D
 
-var combatant_name := "Enemy"
-var max_health := 100.0
-var health := max_health
+var combatant_data: CombatantData
+var health: int
+var max_health: int
+var attack: int
+var display_name: String
 
 func _ready() -> void:
-	$HealthLabel.text = "HP: %s / %s" % [health, max_health]
-	$NameLabel.text = combatant_name
-	
-func show_attacker_highlight() -> void:
-	$AttackerHighlight.show()
-	
-func show_target_highlight() -> void:
-	$TargetHighlight.show()
-	
-func hide_attacker_highlight() -> void:
-	$AttackerHighlight.hide()
-	
-func hide_target_highlight() -> void:
-	$TargetHighlight.hide()
-	
-func take_damage(amount: float) -> void:
+	update_display()
+
+func setup_from_data(data: CombatantData) -> void:
+		var sprite: Sprite2D = $Sprite2D
+		
+		combatant_data = data
+		display_name = data.display_name
+		max_health = data.max_health
+		health = data.max_health
+		attack = data.attack
+		sprite.texture = data.sprite
+		
+func take_damage(amount: int) -> void:
 	health -= amount
-	
-	if health <= 0:
-		health = 0
-		hide()
 		
 	update_display()
-	
+		
 func update_display() -> void:
 	$HealthLabel.text = "HP: %s / %s" % [health, max_health]
---
+++ scripts/combat/combatant_data.gd
@@ -0,0 +1,7 @@
+class_name CombatantData
+extends Resource
+
+@export var display_name: String
+@export var attack: int
+@export var max_health: int
+@export var sprite: Texture2D

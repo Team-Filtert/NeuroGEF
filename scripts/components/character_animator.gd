22940292d181aa7eafa143b0a630a7689d7dcf93
nullptr448 <ghernandezlohaus.78@gmail.com>
2026-02-06 22:01:13 -0300

    extract character animations to resource file

--- scripts/components/character_animator.gd
+++ scripts/components/character_animator.gd
@@ -11,9 +11,9 @@ func play_idle(direction: Vector2) -> void:
 	var animation_name := ""
 	
 	if abs(direction.x) > abs(direction.y):
-		animation_name = "idle_right" if direction.x > 0 else "idle_left"
+		animation_name = "character_animations/idle_right" if direction.x > 0 else "character_animations/idle_left"
 	else:
-		animation_name = "idle_down" if direction.y > 0 else "idle_up"
+		animation_name = "character_animations/idle_down" if direction.y > 0 else "character_animations/idle_up"
 		
 	if animation_player.current_animation != animation_name:
 		animation_player.play(animation_name)
@@ -21,20 +21,16 @@ func play_idle(direction: Vector2) -> void:
 	current_state = State.IDLE
 
 func play_moving(direction: Vector2) -> void:
+	if abs(direction.x) == abs(direction.y) and current_state == State.MOVING:
+		return
+	
 	var animation_name := ""
 	
 	if abs(direction.x) > abs(direction.y):
-		animation_name = "move_right" if direction.x > 0 else "move_left"
-	elif abs(direction.x) < abs(direction.y):
-		animation_name = "move_down" if direction.y > 0 else "move_up"
+		animation_name = "character_animations/move_right" if direction.x > 0 else "character_animations/move_left"
 	else:
-		# Lock current animation if already moving
-		if current_state == State.MOVING:
-			return
-		
-		# Default to horizontal
-		animation_name = "move_right" if direction.x > 0 else "move_left"
-
+		animation_name = "character_animations/move_down" if direction.y > 0 else "character_animations/move_up"
+	
 	if animation_player.current_animation != animation_name:
 		animation_player.play(animation_name)
 	

b2601d62674a05d7e07b4c9347c3f3326c6da942
nullptr448 <ghernandezlohaus.78@gmail.com>
2026-02-07 01:07:44 -0300

    add combat highlights to combatant

--- scripts/combat/combatant.gd
+++ scripts/combat/combatant.gd
@@ -9,17 +9,26 @@ func _ready() -> void:
 	$HealthLabel.text = "HP: %s / %s" % [health, max_health]
 	$NameLabel.text = combatant_name
 	
+func show_attacker_highlight() -> void:
+	$AttackerHighlight.show()
+	
+func show_target_highlight() -> void:
+	$TargetHighlight.show()
+	
+func hide_attacker_highlight() -> void:
+	$AttackerHighlight.hide()
+	
+func hide_target_highlight() -> void:
+	$TargetHighlight.hide()
+	
 func take_damage(amount: float) -> void:
 	health -= amount
 	
 	if health <= 0:
 		health = 0
-		die()
+		hide()
 		
 	update_display()
-		
-func die() -> void:
-	queue_free()
 	
 func update_display() -> void:
 	$HealthLabel.text = "HP: %s / %s" % [health, max_health]

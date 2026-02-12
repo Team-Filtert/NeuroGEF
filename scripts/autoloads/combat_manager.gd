938055fc57d5f7e7e1d2dae3081633da28f5baf9
nullptr448 <ghernandezlohaus.78@gmail.com>
2026-02-12 20:48:44 -0300

    renove unused method

--- scripts/autoloads/combat_manager.gd
+++ scripts/autoloads/combat_manager.gd
@@ -1,32 +1,28 @@
 extends Node
 
-@onready var combat_layer: CanvasLayer = $/root/Root/Combat
-@onready var arena: Arena = $/root/Root/Combat/Arena
+@onready var combat_layer: CanvasLayer = $/root/Root/CombatLayer
+@onready var arena: Arena = $/root/Root/CombatLayer/Arena
 @onready var music_player: AudioStreamPlayer = $/root/Root/MusicPlayer
 
 var battle_theme: AudioStreamOggVorbis = preload("res://assets/audio/battle_bgm_01.ogg")
 
-func start_combat() -> void:  
+func start_combat() -> void:
 	battle_theme.loop = true
 	music_player.stream = battle_theme
 	music_player.play()
 	
 	get_tree().paused = true
-	combat_layer.visible = true
+	combat_layer.show()
 	
-	var enemy_data: CombatantData  = preload("res://resources/combatants/enemy.tres")
+	var enemy_data: CombatantData = load("res://resources/combatants/enemy_base.tres")
 	
 	arena.setup_battle([enemy_data, enemy_data, enemy_data])
 	arena.battle_ended.connect(_on_battle_ended)
 	
-func end_combat() -> void:
+func _on_battle_ended() -> void:
 	music_player.stop()
 	
 	get_tree().paused = false
-	combat_layer.visible = false
-	
+	combat_layer.hide()
 	arena.cleanup_battle()
-	
-func _on_battle_ended() -> void:
-	end_combat()
 	arena.battle_ended.disconnect(_on_battle_ended)

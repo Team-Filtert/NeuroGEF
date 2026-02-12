a4724ccee40f9b9233e66ebd598bebdaf62e4251
nullptr448 <ghernandezlohaus.78@gmail.com>
2026-02-12 20:50:18 -0300

    simplify combatant scene

--- scripts/combat/combatant.gd
+++ scripts/combat/combatant.gd
@@ -1,38 +1,31 @@
 class_name Combatant
 extends Node2D
 
-var combatant_data: CombatantData
-var health: int
+var display_name: String
 var max_health: int
 var attack: int
-var display_name: String
+var health: int
 
-func _ready() -> void:
-	update_display()
-
-func setup_from_data(data: CombatantData) -> void:
-		var sprite: Sprite2D = $Sprite2D
-		
-		combatant_data = data
-		display_name = data.display_name
-		max_health = data.max_health
-		health = data.max_health
-		attack = data.attack
-		sprite.texture = data.sprite
-		
+func setup(data: CombatantData) -> void:
+	var sprite: Sprite2D = $Sprite2D
+	
+	sprite.texture = data.texture
+	display_name = data.display_name
+	max_health = data.max_health
+	attack = data.attack
+	health = data.max_health
+	
+	$DisplayNameLabel.text = display_name
+	$HealthLabel.text = "HP: %s / %s" % [health, max_health]
+	
 func take_damage(amount: int) -> void:
 	health -= amount
+	health = max(0, health)
 	
-	if health <= 0:
+	if health == 0:
 		hide()
 		
-	update_display()
-	
-func focus() -> void:
-	$Focus.show()
-	
-func unfocus() -> void:
-	$Focus.hide()
-		
-func update_display() -> void:
 	$HealthLabel.text = "HP: %s / %s" % [health, max_health]
+	
+func is_alive() -> bool:
+	return health > 0

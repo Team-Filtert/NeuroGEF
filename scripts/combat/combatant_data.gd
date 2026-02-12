992c779ffa2b37d7474622f4fa558193012bfc61
nullptr448 <ghernandezlohaus.78@gmail.com>
2026-02-12 20:50:32 -0300

    add texture to combatant data

--- scripts/combat/combatant_data.gd
+++ scripts/combat/combatant_data.gd
@@ -2,6 +2,6 @@ class_name CombatantData
 extends Resource
 
 @export var display_name: String
-@export var attack: int
 @export var max_health: int
-@export var sprite: Texture2D
+@export var attack: int
+@export var texture: Texture2D

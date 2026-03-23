c8549bcd4f10fbe0c153d4ed8f0589e4f25394cc
Programmer-Marco <marcoggop19@gmail.com>
2026-03-23 02:47:07 -0400

    Update queue_enemy_actions_state.gd

--- scripts/combat/arena_components/states/queue_enemy_actions_state.gd
+++ scripts/combat/arena_components/states/queue_enemy_actions_state.gd
@@ -16,30 +16,95 @@ func _queue_enemy_actions() -> void:
 	for enemy in alive_enemies:
 		
 		var action = CombatantAction.new()
-		action.type = CombatantAction.Type.ATTACK
 		action.source = enemy
 		
+		var all_skills : Dictionary = CombatantAction.action_dict
+		var enemy_actions : Array = [enemy.action_slot1,enemy.action_slot2,enemy.action_slot3]
 		var points : Array[float]
 		var points_total : float = 0
 		var percentag : float = 0
 		var i : int = 0
+		var skill : Dictionary
+		var highest : float = 0
+		var temp_int : int
 		
-		var temp_alive_party = alive_party
-		temp_alive_party.shuffle()
 		
-		for ally in temp_alive_party:
-			var hp_done = ally.health/ally.max_health
-			var attack_done = ally.base_attack/99#change to max attack when made
+		#picking skill to use
+		for j in range(enemy_actions.size()):
 			
-			points.append(hp_done + attack_done) 
+			match enemy_actions[j]:
+				CombatantAction.Action.BLANK:
+					points.append(0)
+				CombatantAction.Action.BASIC_ATTACK:
+					for k in range(alive_party.size()):
+						if (alive_party[k].health + alive_party[k].base_defense - enemy.base_attack * all_skills[enemy_actions[j]].BASE_DAMAGE <= 0):
+							points.append(25)
+						else:
+							points.append(5)
+				CombatantAction.Action.BASIC_HEAL:
+					for k in range(alive_enemies.size()):
+						if (alive_enemies[k].health - enemy.base_magic * all_skills[enemy_actions[j]].BASE_HEALING <= 0):
+							points.append(10)
+						else:
+							points.append(3)
+		
+		
+		for j in range(enemy_actions.size()):
 			
-			points_total = points_total + points[i]
-			i += 1
-			
-		var random_number = randf_range(0,points_total)
-		for j in range(alive_party.size()):
-			percentag += points[j]
-			if  random_number <= percentag:
-				action.target = temp_alive_party[j]
-				parent.action_queue.append(action)
-				break 
+			if (points[j] >= highest):
+				highest = points[j]
+				temp_int = j
+		points.clear()
+		skill = all_skills[enemy_actions[temp_int]]
+		action.type = skill.TYPE
+		
+		
+		
+		
+		match skill.TYPE:
+			CombatantAction.Type.NONE:
+				print()
+			CombatantAction.Type.ATTACK:
+				var temp_alive_party = alive_party
+				temp_alive_party.shuffle()
+				
+				for ally in temp_alive_party:
+					var hp_done = (ally.health/ally.max_health - skill.TARGET_LOW_HP) * -abs(skill.HP_WEIGHT) if skill.TARGET_LOW_HP else abs(skill.HP_WEIGHT) 
+					 #change to max attack instead of 99
+					var attack_done = (ally.base_attack/99 - skill.TARGET_LOW_ATTACK) * -abs(skill.ATTACK_WEIGHT) if skill.TARGET_LOW_ATTACK else abs(skill.ATTACK_WEIGHT)
+					
+					
+					points.append(hp_done + attack_done) 
+					
+					points_total = points_total + points[i]
+					i += 1
+					
+				var random_number = randf_range(0,points_total)
+				for j in range(alive_party.size()):
+					percentag += points[j]
+					if  random_number <= percentag:
+						action.target = temp_alive_party[j]
+						parent.action_queue.append(action)
+						break 
+			CombatantAction.Type.HEAL:
+				print("in progress")
+		
+	
+		#type      weights & target slecction   display name    base values  
+		#"TYPE": int
+		#"HP_WEIGHT": float
+		#"TARGET_LOW_HP": bool
+		#"ATTACK_WEIGHT": float
+		#"TARGET_LOW_ATTACK": bool
+		#"DISPLAY_NAME": string
+		#"BASE_DAMAGE": float
+		#"BASE_HEALING": float
+
+
+
+
+# heat needs to be added first
+		#if (hp_done + attack_done - global.Get(heat) > 0)
+		  #points[i] = hp_done + attack_done
+		#else
+		  #points[i] = 1

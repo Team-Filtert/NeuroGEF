d817101eacc132ddb81288286a0bf3c073fd044a
Programmer-Marco <marcoggop19@gmail.com>
2026-03-23 17:37:36 -0400

    Update combatant_action.gd

--- scripts/combat/combatant_action.gd
+++ scripts/combat/combatant_action.gd
@@ -14,17 +14,19 @@ enum Action {
 	BASIC_HEAL = 2
 }
 
-
 #type      weights & target slecction   display name    base values  
-#"TYPE": int
-#"HP_WEIGHT": float
-#"TARGET_LOW_HP": bool
-#"ATTACK_WEIGHT": float
-#"TARGET_LOW_ATTACK": bool
-#"DISPLAY_NAME": string
-#"BASE_DAMAGE": float
-#"BASE_HEALING": float
-#var skill = CombatantAction.action_dict[CombatantAction.Action.BASIC_ATTACK]
+enum Action_data {
+	TYPE,#: int
+	HP_WEIGHT,# float
+	TARGET_LOW_HP,# bool
+	ATTACK_WEIGHT,# float
+	TARGET_LOW_ATTACK,# bool
+	DISPLAY_NAME,# string
+	BASE_DAMAGE,# float
+	BASE_HEALING# float
+}
+
+#var skill = CombatantAction.action_dict[CombatantAction.Action.BASIC_ATTACK][Action_data.TYPE]
 #AI logic is in QueueEnemyActionsState
 
 static var action_dict: Dictionary = {
@@ -32,22 +34,22 @@ static var action_dict: Dictionary = {
 	"TYPE": Type.NONE
 	},
 	Action.BASIC_ATTACK: {
-	"TYPE": Type.ATTACK, 
-	"HP_WEIGHT": 1.0,
-	"TARGET_LOW_HP": true, 
-	"ATTACK_WEIGHT": 0.0,
-	"TARGET_LOW_ATTACK": true,
-	"DISPLAY_NAME": "Coolest attack",
-	"BASE_DAMAGE": 1.0
+	Action_data.TYPE : Type.ATTACK, 
+	Action_data.HP_WEIGHT: 1.5,
+	Action_data.TARGET_LOW_HP: true, 
+	Action_data.ATTACK_WEIGHT: 1.0,
+	Action_data.TARGET_LOW_ATTACK: true,
+	Action_data.DISPLAY_NAME: "Coolest attack",
+	Action_data.BASE_DAMAGE: 1.0
 	},
 	Action.BASIC_HEAL: {
-	"TYPE": Type.HEAL, 
-	"HP_WEIGHT": 4.0,
-	"TARGET_LOW_HP": true, 
-	"ATTACK_WEIGHT": 0.0,
-	"TARGET_LOW_ATTACK": true,
-	"DISPLAY_NAME": "Coolest heal",
-	"BASE_HEALING": 1.0
+	Action_data.TYPE: Type.HEAL, 
+	Action_data.HP_WEIGHT: 4.0,
+	Action_data.TARGET_LOW_HP: true, 
+	Action_data.ATTACK_WEIGHT: 0.0,
+	Action_data.TARGET_LOW_ATTACK: true,
+	Action_data.DISPLAY_NAME: "Coolest heal",
+	Action_data.BASE_HEALING: 1.0
 	}
 	
 }

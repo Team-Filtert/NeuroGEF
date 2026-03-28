b3ac214b0bfca85a28236377100a8224f71cc834
Programmer-Marco <marcoggop19@gmail.com>
2026-03-28 18:09:03 -0400

    Update combatant_action.gd

--- scripts/combat/combatant_action.gd
+++ scripts/combat/combatant_action.gd
@@ -1,61 +1,10 @@
 class_name CombatantAction
 extends RefCounted
 
-enum Type {
-	NONE,
-	ATTACK,
-	BLOCK,
-	HEAL
-}
 
-enum Action {
-	BLANK = 0,
-	BASIC_ATTACK = 1,
-	BASIC_HEAL = 2
-}
-
-#type      weights & target slecction   display name    base values  
-enum Action_data {
-	TYPE,#: int
-	HP_WEIGHT,# float
-	TARGET_LOW_HP,# bool
-	ATTACK_WEIGHT,# float
-	TARGET_LOW_ATTACK,# bool
-	DISPLAY_NAME,# string
-	BASE_DAMAGE,# float
-	BASE_HEALING# float
-}
-
-#var skill = CombatantAction.action_dict[CombatantAction.Action.BASIC_ATTACK][Action_data.TYPE]
-#AI logic is in QueueEnemyActionsState
-
-static var action_dict: Dictionary = {
-	Action.BLANK: {
-	"TYPE": Type.NONE
-	},
-	Action.BASIC_ATTACK: {
-	Action_data.TYPE : Type.ATTACK, 
-	Action_data.HP_WEIGHT: 1.5,
-	Action_data.TARGET_LOW_HP: true, 
-	Action_data.ATTACK_WEIGHT: 1.0,
-	Action_data.TARGET_LOW_ATTACK: true,
-	Action_data.DISPLAY_NAME: "Coolest attack",
-	Action_data.BASE_DAMAGE: 1.0
-	},
-	Action.BASIC_HEAL: {
-	Action_data.TYPE: Type.HEAL, 
-	Action_data.HP_WEIGHT: 4.0,
-	Action_data.TARGET_LOW_HP: true, 
-	Action_data.ATTACK_WEIGHT: 0.0,
-	Action_data.TARGET_LOW_ATTACK: true,
-	Action_data.DISPLAY_NAME: "Coolest heal",
-	Action_data.BASE_HEALING: 1.0
-	}
-	
-}
 
 var display_name: String
-var type: Type
+var type: CombatantActionStorage.Type
 var source: Combatant
 var target: Combatant
 

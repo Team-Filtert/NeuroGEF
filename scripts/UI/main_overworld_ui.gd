139d5aac193e53f3de65dcc336eb2d146bbf8bbb
LukasDerBaum <137618724+LukasDerBaum42@users.noreply.github.com>
2026-03-30 21:47:11 +0200

    finsh the overworld ui
    finishd it

--- scripts/UI/main_overworld_ui.gd
+++ scripts/UI/main_overworld_ui.gd
@@ -2,11 +2,13 @@ extends Control
 
 
 var is_expanded = false
-
+var input_handler: InputComponent
+signal main_ui_interaction_triggered
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-
+	input_handler = InputComponent.new()
+	
 	if CombatManager.is_in_combat:
 		hide()
 	elif is_expanded:
@@ -15,25 +17,32 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	main_ui_interaction_triggered.connect(_on_togle_menu)
+	
 	if CombatManager.is_in_combat:
 		hide()
 	elif is_expanded:
 		show()
 		
-func _input(event: InputEvent) -> void:
-	if event is InputEventKey and event.pressed:
-		if event.keycode == KEY_E:
-			if is_expanded:
-				collapse_menu()
-			else:
-				expand_menu()
-		else:
-			collapse_menu()
+			
+func _unhandled_input(event: InputEvent) -> void:
+	if input_handler.get_interact_input(event):
+		main_ui_interaction_triggered.emit()
 
-func expand_menu():
-	is_expanded = true
-	show()
+func _on_togle_menu():
+	if is_expanded:
+		is_expanded = false
+		hide()
+	else:
+		is_expanded = true
+		show()
 
-func collapse_menu():
-	is_expanded = false
-	hide()
+#func _input(event: InputEvent) -> void:
+#	if event is InputEventKey and event.pressed:
+#		if event.keycode == KEY_E:
+#			if is_expanded:
+#				_on_collapse_menu()
+#			else:
+#				_on_expand_menu()
+#		else:
+#			_on_collapse_menu()

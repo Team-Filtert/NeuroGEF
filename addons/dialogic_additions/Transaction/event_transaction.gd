051a65b1dc7ab705a34c9667918707dcbe0bdc13
komikronsgit <cusworthkalvin@gmail.com>
2026-03-31 00:29:23 -0400

    switched to enums

--- addons/dialogic_additions/Transaction/event_transaction.gd
+++ addons/dialogic_additions/Transaction/event_transaction.gd
@@ -3,13 +3,14 @@ extends DialogicEvent
 class_name DialogicTransactionEvent
 
 # Define properties of the event here
-var item_type: Item
+var item: Item
+var item_type: Inventory.ItemType
 var item_amount := 0
 var money_amount := 0
 
 func _execute() -> void:
 	# This will execute when the event is reached
-	Inventory.perform_transaction(item_type, item_amount, money_amount)
+	Inventory.perform_transaction(item, item_amount, money_amount)
 	finish() # called to continue with the next event
 
 
@@ -33,7 +34,8 @@ func get_shortcode() -> String:
 
 func get_shortcode_parameters() -> Dictionary:
 	return {
-		'item_type' : {"property": "item_type", "default": null},
+		'item' : {"property": "item", "default": null},
+		'item_type' : {"property": "item_type", "default": Inventory.ItemType.NONE},
 		'item_amount' : {"property": "item_amount", "default": 0},
 		'money_amount' : {"property": "money_amount", "default": 0},
 	}
@@ -47,7 +49,25 @@ func get_shortcode_parameters() -> Dictionary:
 
 func build_event_editor() -> void:
 	add_header_edit("item_amount", ValueType.NUMBER, {'left_text':'Exchange'})
-	add_header_edit("item_type", ValueType.FILE)
+	add_header_edit("item", ValueType.FILE)
+	add_header_edit('item_type',ValueType.FIXED_OPTIONS, {'options': [
+		{
+			'label': 'wepon',
+			'value': Inventory.ItemType.WEPON,
+		},
+		{
+			'label': 'armor',
+			'value': Inventory.ItemType.ARMOR,
+		},
+		{
+			'label': 'collectable',
+			'value': Inventory.ItemType.COLLECTABLE,
+		},
+		{
+			'label': 'consumable',
+			'value': Inventory.ItemType.CONSUMABLE,
+		},
+	]})
 	add_header_edit("money_amount", ValueType.NUMBER, {'left_text':'for'})
 
 #endregion
--- scripts/autoloads/inventory.gd
+++ scripts/autoloads/inventory.gd
@@ -1,5 +1,13 @@
 extends Node
 
+enum ItemType {
+	NONE,
+	WEPON,
+	ARMOR,
+	COLLECTABLE,
+	CONSUMABLE,
+}
+
 var weapons: Array[ItemStack] = []
 var armors: Array[ItemStack] = []
 var collectables: Array[ItemStack] = []
@@ -7,15 +15,24 @@ var consumables: Array[ItemStack] = []
 
 var money := 0
 
-func perform_transaction(item_type: Item, item_amount: int, money_amount: int):
-	var items := get_items(item_type)
+func perform_transaction(
+	item: Item = null,
+	item_type: ItemType = ItemType.NONE,
+	item_amount: int = 0,
+	money_amount: int = 0
+	):
 	money += money_amount
+	if item_amount != 0:
+		add_item(item, item_type, item_amount)
+
+func add_item(item: Item, item_type: ItemType, item_amount: int):
+	var items := get_items(item_type)
 	var index := items.find_custom(
-		func(i:ItemStack): return i.item.display_name == item_type.display_name
+		func(i:ItemStack): return i.item.display_name == item.display_name
 	)
 	if index == -1:
 		var stack := ItemStack.new()
-		stack.item = item_type
+		stack.item = item
 		stack.amount = item_amount
 		items.append(stack)
 	else:
@@ -23,12 +40,16 @@ func perform_transaction(item_type: Item, item_amount: int, money_amount: int):
 		if items[index].amount < 1:
 			items.remove_at(index)
 
-func get_items(item_type: Item) -> Array[ItemStack]:
-	if item_type is ItemWepon:
-		return weapons
-	elif item_type is ItemArmor:
-		return armors
-	elif item_type is ItemCollectable:
-		return collectables
-	else:
-		return consumables
+func get_items(item_type: ItemType) -> Array[ItemStack]:
+	match item_type:
+		ItemType.WEPON:
+			return weapons
+		ItemType.ARMOR:
+			return armors
+		ItemType.COLLECTABLE:
+			return collectables
+		ItemType.CONSUMABLE:
+			return consumables
+		_:
+			printerr("uknown item type")
+			return collectables

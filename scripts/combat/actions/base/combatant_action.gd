34922278284206aaf823fe515823d9fbe65d3216
lokt02 <lokt02@mail.ru>
2026-04-03 17:27:01 +0300

    feat: added combo actions

--- scripts/combat/actions/base/combatant_action.gd
+++ scripts/combat/actions/base/combatant_action.gd
@@ -18,9 +18,14 @@ func animate():
 # for processing action in UI
 var process_func: Callable
 
-
+## This value will be used for various
+## purposes, most simple and straightforward
+## example is damage calculated for an attack
 func get_value():
 	return 0
 
+## Called on action execution, i. e.
+## when player pressed corresponding
+## button
 func action_result():
 	pass
---
+++ scripts/combat/actions/basic_combo_attack.gd
@@ -0,0 +1,29 @@
+class_name BasicComboAttack
+extends CombatantComboAction
+
+func animate():
+	var original_pos = source.position
+	var target_pos = target.position
+
+	var tween = source.create_tween()
+	tween.tween_property(source, "position", target_pos, 0.3)
+	await tween.finished
+
+	await _handle_attack_action()
+
+	tween = source.create_tween()
+	tween.tween_property(source, "position", original_pos, 0.3)
+	await tween.finished
+
+
+@export var attack_multiplier: float = 1.0
+@export var mana_cost: int = 0
+
+func _handle_attack_action():
+	action_result()
+
+func get_value():
+	return self.source.get_attack() * attack_multiplier
+
+func action_result():
+	target.take_damage(get_value())

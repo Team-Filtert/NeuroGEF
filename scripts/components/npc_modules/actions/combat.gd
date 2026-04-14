145a0f6219ef9852bbcefce9b9b1cc014611f387
komikronsgit <cusworthkalvin@gmail.com>
2026-04-14 00:59:52 -0700

    feat: added new templates

--- scripts/components/npc_modules/actions/combat.gd
+++ scripts/components/npc_modules/actions/combat.gd
@@ -1,8 +1,10 @@
 class_name NpcActionCombat
 extends NpcActionBase
 
-@export var enemy_combatants: Array[CombatantData]
+@export var enemy_combatant1: CombatantData
+@export var enemy_combatant2: CombatantData
+@export var enemy_combatant3: CombatantData
 @export var music: AudioStream
 
 func _preform_action():
-	CombatManager.start_combat(enemy_combatants, music)
+	CombatManager.start_combat([enemy_combatant1, enemy_combatant2, enemy_combatant3], music)

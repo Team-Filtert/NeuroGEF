179b38dc5c2d335fe5ad45fbe2ffadbd0ac9ddb9
komikronsgit <cusworthkalvin@gmail.com>
2026-04-14 01:20:21 -0700

    fix: add music to npc combat again

--- scripts/npc_modules/actions/combat.gd
+++ scripts/npc_modules/actions/combat.gd
@@ -4,6 +4,7 @@ extends NpcActionBase
 @export var enemy_combatant1: CombatantData
 @export var enemy_combatant2: CombatantData
 @export var enemy_combatant3: CombatantData
+@export var music: AudioStream
 
 func _preform_action():
-	CombatManager.start_combat([enemy_combatant1, enemy_combatant2, enemy_combatant3])
+	CombatManager.start_combat([enemy_combatant1, enemy_combatant2, enemy_combatant3], music)

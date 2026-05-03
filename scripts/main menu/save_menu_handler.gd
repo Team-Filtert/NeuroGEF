d600035c3f5ae020900260b4d6d9aea28bb5304b
komikronsgit <cusworthkalvin@gmail.com>
2026-05-02 20:40:36 -0400

    refactor: update starting save file

--- scripts/main menu/save_menu_handler.gd
+++ scripts/main menu/save_menu_handler.gd
@@ -84,24 +84,40 @@ func start_game_in_slot(save_slot: int):
 	
 	var starting_player_combat_data_path := "res://resources/combatants/player_base.tres"
 	var starting_player_combat_data := load(starting_player_combat_data_path)
+	var party_member_combat_data_path := "res://resources/combatants/party_member_base.tres"
+	var party_member_combat_data := load(party_member_combat_data_path)
 	GameManager.load_state(starting_scene_filepath, {
 		"pos_x": 0,
 		"pos_y": 0,
 		},
 		["res://scenes/characters/player.tscn"],
-		[{
-			"path": starting_player_combat_data_path,
-			"display_name": starting_player_combat_data.display_name,
-			"texture_path": starting_player_combat_data.texture.resource_path,
-			"max_health": starting_player_combat_data.max_health,
-			"health": starting_player_combat_data.health,
-			"max_mana": starting_player_combat_data.max_mana,
-			"mana": starting_player_combat_data.mana,
-			"base_attack": starting_player_combat_data.base_attack,
-			"base_speed": starting_player_combat_data.base_speed,
-			"base_defense": starting_player_combat_data.base_defense
-		}], 0,
-		[], [], [], [])
+		[
+			{
+				"path": starting_player_combat_data_path,
+				"display_name": starting_player_combat_data.display_name,
+				"texture_path": starting_player_combat_data.texture.resource_path,
+				"max_health": starting_player_combat_data.max_health,
+				"health": starting_player_combat_data.health,
+				"max_mana": starting_player_combat_data.max_mana,
+				"mana": starting_player_combat_data.mana,
+				"base_attack": starting_player_combat_data.base_attack,
+				"base_speed": starting_player_combat_data.base_speed,
+				"base_defense": starting_player_combat_data.base_defense
+			},
+			{
+				"path": party_member_combat_data_path,
+				"display_name": party_member_combat_data.display_name,
+				"texture_path": party_member_combat_data.texture.resource_path,
+				"max_health": party_member_combat_data.max_health,
+				"health": party_member_combat_data.health,
+				"max_mana": party_member_combat_data.max_mana,
+				"mana": party_member_combat_data.mana,
+				"base_attack": party_member_combat_data.base_attack,
+				"base_speed": party_member_combat_data.base_speed,
+				"base_defense": party_member_combat_data.base_defense
+			}
+		],
+		0, [], [], [], [])
 	
 	SaveManager.save(save_slot)
 	SceneManager.current_scene_init()

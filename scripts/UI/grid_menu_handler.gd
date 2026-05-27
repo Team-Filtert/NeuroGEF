ccac2432966be1846b8336d96c6e4c6594007f56
LukasDerBaum <137618724+LukasDerBaum42@users.noreply.github.com>
2026-05-27 20:13:08 +0200

    Improved the combat UI
    made it so that in the combat UI with the grid it adds new rows automatily, and you can "scroll" if there are to many elements, also fixed the translation

--- scripts/UI/grid_menu_handler.gd
+++ scripts/UI/grid_menu_handler.gd
@@ -2,21 +2,23 @@ extends MenuHandler
 class_name GridMenuHandler
 
 @export var is_cycled: bool = true
+@export var view_hight: int = 3
 
-var columns: Array[VBoxContainer]
-var row_count: int
+var rows: Array[HBoxContainer]
+var column_count: int
+var view_pos : int = 0
 
 func _ready() -> void:
 	parent = get_parent() as Control
 
 	var children = parent.get_children()
-	columns = []
+	rows = []
 	for child in children:
-		if child is VBoxContainer:
-			columns.append(child)
+		if child is HBoxContainer:
+			rows.append(child)
 
-	if columns.size() > 0:
-		row_count = columns[0].get_children().filter(func(child):
+	if rows.size() > 0:
+		column_count = rows[0].get_children().filter(func(child):
 			return child is MenuElement
 		).size()
 
@@ -32,18 +34,19 @@ func _ready() -> void:
 ## Getting menu items from handled menu
 func get_items() -> Array[MenuElement]:
 	var items: Array[MenuElement] = []
-	for column in columns:
-		for row_ndx in row_count:
+	for row in rows:
+		for column_ndx in range(column_count):
 			# pushing null to preserve grid structure
 			# to simplify focus navigation logic
 
-			if row_ndx >= column.get_child_count():
+			if column_ndx >= row.get_child_count():
 				items.append(null)
 				continue
 
-			var row = column.get_child(row_ndx)
-			if row is MenuElement:
-				items.append(row)
+			var column = row.get_child(column_ndx)
+			if column is MenuElement:
+				
+				items.append(column)
 			else:
 				items.append(null)
 	return items
@@ -51,28 +54,28 @@ func get_items() -> Array[MenuElement]:
 #region Grid navigation logic
 
 func _get_item_at_position(column: int, row: int) -> MenuElement:
-	if column < 0:
-		if is_cycled:
-			column += columns.size()
-		else:
-			return null
-	elif column >= columns.size():
-		if is_cycled:
-			column = 0
-		else:
-			return null
-
 	if row < 0:
 		if is_cycled:
-			row += row_count
+			row += rows.size()
 		else:
 			return null
-	elif row >= row_count:
+	elif row >= rows.size():
 		if is_cycled:
 			row = 0
 		else:
 			return null
 
+	if column < 0:
+		if is_cycled:
+			column += column_count
+		else:
+			return null
+	elif column >= column_count:
+		if is_cycled:
+			column = 0
+		else:
+			return null
+	
 	var index = _grid_position_to_arr_index(column, row)
 	var items = get_items()
 	if index < items.size():
@@ -81,36 +84,36 @@ func _get_item_at_position(column: int, row: int) -> MenuElement:
 
 func _get_item_upper(column: int, row: int) -> MenuElement:
 	var item = _get_item_at_position(column, row - 1)
-	if not item:
+	if not item and is_cycled:
 		return _get_item_upper(column, row - 1)
 	return item
 
 func _get_item_lower(column: int, row: int) -> MenuElement:
 	var item = _get_item_at_position(column, row + 1)
-	if not item:
+	if not item and is_cycled:
 		return _get_item_lower(column, row + 1)
 	return item
 
 func _get_item_left(column: int, row: int) -> MenuElement:
 	var item = _get_item_at_position(column - 1, row)
-	if not item:
+	if not item and is_cycled:
 		return _get_item_left(column - 1, row)
 	return item
 
 func _get_item_right(column: int, row: int) -> MenuElement:
 	var item = _get_item_at_position(column + 1, row)
-	if not item:
+	if not item and is_cycled:
 		return _get_item_right(column + 1, row)
 	return item
 
 func _arr_indxes_to_grid_position(index: int) -> Vector2i:
+	var column = index % column_count
 	@warning_ignore("integer_division")
-	var column = index / row_count
-	var row = index % row_count
+	var row = index / column_count
 	return Vector2i(column, row)
 
 func _grid_position_to_arr_index(column: int, row: int) -> int:
-	return column * row_count + row
+	return row * column_count + column
 
 #endregion
 
@@ -119,28 +122,57 @@ func _grid_position_to_arr_index(column: int, row: int) -> int:
 # 	return _get_item_on_position(pos.x, pos.y)
 
 func clear_items() -> void:
-	for column in columns:
-		for row_ndx in row_count:
-			var item = _get_item_at_position(columns.find(column), row_ndx)
+	for row in rows:
+		for column_ndx in range(column_count):
+			var item = _get_item_at_position(column_ndx, rows.find(row))
 			if item:
 				item.queue_free()
 
 func add_item(item: MenuElement) -> void:
 	var items_size = get_items().filter(func(i): return i != null).size()
 	var pos = _arr_indxes_to_grid_position(items_size)
-	if pos.x >= columns.size():
+	if pos.x >= column_count:
 		printerr("No more columns to add items to")
 		return
-	elif pos.y >= row_count:
-		printerr("No more rows to add items to")
-		return
-	columns[pos.x].add_child(item)
+	elif pos.y >= rows.size():
+		# printerr("No more rows to add items to")
+		var new_row := HBoxContainer.new()
+		rows.append(new_row)
+		# self.add_sibling(new_row)
+		self.get_parent().add_child(new_row)
+		
+	rows[pos.y].add_child(item)
 
 func create_items(actions: Array[CombatantAction], on_item_pressed: Callable) -> void:
 	for action in actions:
 		var item = MenuElement.create(action.display_name, self, action)
 		item.pressed.connect(on_item_pressed.bind(action))
 		add_item(item)
+
+func hide_rows(row:int) -> void:
+	if row <= view_pos:
+		view_pos = row-1
+		if view_pos < 0 : view_pos = 0 
+
+	elif row >= view_pos + view_hight - 1:
+		view_pos = row - view_hight  + 2
+		if view_pos > rows.size()-view_hight : view_pos = rows.size()-view_hight 
+
+	for i in range(rows.size()):
+		if i >= view_pos and i < view_pos + view_hight:
+			rows[i].show()
+		else:
+			rows[i].hide()
+
+func _on_gui_focus_changed(item: Control) -> void:
+	
+	var items := get_items()
+	var index := items.find(item)
+	
+	var pos := _arr_indxes_to_grid_position(index)
+	
+	hide_rows(pos.y)
+
 
 # Implementation for grid layout out of columns as VBoxContainers
 # while main container is HBoxContainer
@@ -164,11 +196,11 @@ func build_navigation() -> void:
 		var top_item = _get_item_upper(column, row)
 		var bottom_item = _get_item_lower(column, row)
 
-		item.focus_neighbor_left = left_item.get_path() if left_item else item.get_path()
-		item.focus_neighbor_right = right_item.get_path() if right_item else item.get_path()
-		item.focus_neighbor_top = top_item.get_path() if top_item else item.get_path()
-		item.focus_neighbor_bottom = bottom_item.get_path() if bottom_item else item.get_path()
+		item.focus_neighbor_left = item.get_path_to(left_item) if left_item else item.get_path()
+		item.focus_neighbor_right = item.get_path_to(right_item) if right_item else item.get_path()
+		item.focus_neighbor_top = item.get_path_to(top_item) if top_item else item.get_path()
+		item.focus_neighbor_bottom = item.get_path_to(bottom_item) if bottom_item else item.get_path()
 
 		if i == 0:
 			item.grab_focus()
-	
+			#hide_rows(row)

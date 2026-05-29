fab23949be2c5c627bdb518c42fef9a86f46e88e
Gabriel Hernández <ghernandezlohaus.78@gmail.com>
2026-05-28 21:07:13 -0400

    feat: ui management

---
+++ autoloads/ui_manager.gd
@@ -0,0 +1,32 @@
+extends Node
+
+var ui_layer: CanvasLayer
+var ui_stack: Array[Node] = []
+
+func push_ui(scene: PackedScene) -> void:
+	# TODO: Do something about this being hardcoded
+	if not ui_stack.is_empty():
+		ui_stack.back().hide()
+		
+	var node := scene.instantiate()
+
+	ui_layer.add_child(node)
+	ui_stack.append(node)
+
+func pop_ui() -> void:
+	if ui_stack.is_empty():
+		return
+
+	ui_stack.back().queue_free()
+	ui_stack.pop_back()
+	
+	# TODO: Do something about this being hardcoded
+	if not ui_stack.is_empty():
+		ui_stack.back().show()
+
+func clear_ui() -> void:
+	if ui_stack.is_empty():
+		return
+
+	for node in ui_stack:
+		node.queue_free()

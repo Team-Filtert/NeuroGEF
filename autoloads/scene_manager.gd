15711a624dd09de9ab27aeb55e01365eef4e14c4
Gabriel Hernández <ghernandezlohaus.78@gmail.com>
2026-05-30 00:37:58 -0400

    fix: defer scene change

--- autoloads/scene_manager.gd
+++ autoloads/scene_manager.gd
@@ -4,6 +4,9 @@ var scene_container: Node
 
 # TODO: Add some sort of validation to chech if the resource path is valid
 func change_scene_to(scene: PackedScene) -> void:
+	call_deferred("_do_change_scene", scene)
+	
+func _do_change_scene(scene: PackedScene) -> void:
 	clear_scene()
 	scene_container.add_child(scene.instantiate())
 
--- components/scene_trigger.gd
+++ components/scene_trigger.gd
@@ -9,10 +9,11 @@ extends Area2D
 		var shape_node := get_node_or_null("CollisionShape2D")
 		if shape_node and shape_node.shape:
 			shape_node.shape.size = value
+			
 
 func _ready() -> void:
 	trigger_size = trigger_size
 
-func _on_body_entered(body: Node) -> void:
+func _on_body_entered(_body: Node) -> void:
 	assert(target_scene, "Target scene not set")
 	SceneManager.change_scene_to(load(target_scene))

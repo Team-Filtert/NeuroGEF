1ae4765da0601694fd0cc58257aaa5025e795b53
Gabriel Hernández <ghernandezlohaus.78@gmail.com>
2026-05-30 00:53:09 -0400

    fix: remove automatic setup in favor of adding the collision shape manually

--- components/scene_trigger.gd
+++ components/scene_trigger.gd
@@ -1,19 +1,7 @@
-@tool
 extends Area2D
 
 @export_file("*.tscn") var target_scene: String
 
-@export var trigger_size := Vector2(32, 32):
-	set(value):
-		trigger_size = value
-		var shape_node := get_node_or_null("CollisionShape2D")
-		if shape_node and shape_node.shape:
-			shape_node.shape.size = value
-			
-
-func _ready() -> void:
-	trigger_size = trigger_size
-
-func _on_body_entered(_body: Node) -> void:
+func _on_body_entered(_body: Node2D) -> void:
 	assert(target_scene, "Target scene not set")
 	SceneManager.change_scene_to(load(target_scene))

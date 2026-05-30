fe657dd90105de20a7eec46b03eaf907a2c6c1e3
Gabriel Hernández <ghernandezlohaus.78@gmail.com>
2026-05-30 01:13:58 -0400

    chore: comment

--- components/scene_trigger.gd
+++ components/scene_trigger.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 @export_file("*.tscn") var target_scene: String
 
+# TODO: Check if the body is a player
 func _on_body_entered(_body: Node2D) -> void:
 	assert(target_scene, "Target scene not set")
 	SceneManager.change_scene_to(load(target_scene))

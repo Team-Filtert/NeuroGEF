e331f21a6def32fa23381d57e407e3f3c58721af
Gabriel Hernández <ghernandezlohaus.78@gmail.com>
2026-05-30 00:24:31 -0400

    feat: scene trigger

---
+++ components/scene_trigger.gd
@@ -0,0 +1,18 @@
+@tool
+extends Area2D
+
+@export_file("*.tscn") var target_scene: String
+
+@export var trigger_size := Vector2(32, 32):
+	set(value):
+		trigger_size = value
+		var shape_node := get_node_or_null("CollisionShape2D")
+		if shape_node and shape_node.shape:
+			shape_node.shape.size = value
+
+func _ready() -> void:
+	trigger_size = trigger_size
+
+func _on_body_entered(body: Node) -> void:
+	assert(target_scene, "Target scene not set")
+	SceneManager.change_scene_to(load(target_scene))

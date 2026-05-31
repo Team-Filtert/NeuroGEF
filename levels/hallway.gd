b39f5dec5b3d3246c75ae5d6d025dd5b4bf073a1
Gabriel Hernández <ghernandezlohaus.78@gmail.com>
2026-05-31 16:55:10 -0400

    test: simultaneous sound effects

--- levels/hallway.gd
+++ levels/hallway.gd
@@ -2,3 +2,5 @@ extends Node2D
 
 func _ready() -> void:
 	AudioManager.stop_bgm()
+	AudioManager.play_sfx(preload("res://assets/test_sfx_1.wav"))
+	AudioManager.play_sfx(preload("res://assets/test_sfx_2.wav"))

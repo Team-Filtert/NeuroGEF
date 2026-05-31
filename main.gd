aff7d414f035ac60f3adec94153359e75f97afe8
Gabriel Hernández <ghernandezlohaus.78@gmail.com>
2026-05-30 23:33:35 -0400

    chore: import files

--- main.gd
+++ main.gd
@@ -2,6 +2,7 @@ extends Node
 
 func _ready() -> void:
 	SceneManager.scene_container = $SceneContainer
+	AudioManager.bgm_player = $BGMPlayer
 	UIManager.ui_layer = $UILayer
 	UIManager.push_ui(preload("res://ui/main_menu.tscn"))
 

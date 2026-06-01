0411719251eb92b160daa612f520ee1d842a6e66
Gabriel Hernández <ghernandezlohaus.78@gmail.com>
2026-05-31 21:55:37 -0400

    chore: fix typo

--- autoloads/scene_manager.gd
+++ autoloads/scene_manager.gd
@@ -4,7 +4,7 @@ var scene_container: Node
 
 # TODO: Add some sort of validation to check if the resource path is valid
 func change_scene_to(scene: PackedScene) -> void:
-	# Use call_deferred() for some reason I don't understand, something to do with physics flushing or something'
+	# Use call_deferred() for some reason I don't understand, something to do with physics flushing or something
 	# It did the trick at least
 	call_deferred("_do_change_scene", scene)
 	

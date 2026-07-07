38d9080a0a38adbbdeab8ba25a0bda0aa194b799
Gabriel Hernández <ghernandezlohaus.78@gmail.com>
2026-07-07 15:08:17 -0400

    fix: update autoload name

--- main.gd
+++ main.gd
@@ -1,7 +1,7 @@
 extends Node
 
 func _ready() -> void:
-	Global.init(%PlayerRoot)
+	Game.init(%PlayerRoot)
 	LevelManager.init(%LevelRoot, %TransitionRoot)
-	Global.spawn_player()
+	Game.spawn_player()
 	LevelManager.change_level("res://levels/starting/home/bedroom.tscn", "default")

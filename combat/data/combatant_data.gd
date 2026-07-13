e9dbc4cca63dc185e7b848a6fe3cb3442cf79996
komikronsgit <cusworthkalvin@gmail.com>
2026-07-13 16:21:19 -0400

    feat: conbat data

---
+++ combat/data/combatant_data.gd
@@ -0,0 +1,14 @@
+class_name CombatantData
+extends RefCounted
+
+var texture: Texture2D
+
+var max_health: int
+var health: int
+var max_mana
+var mana: int
+var attack: int
+var magic: int
+var defense: int
+var speed: int
+var accuracy: int
---
+++ combat/data/enemy_data.gd
@@ -0,0 +1,5 @@
+class_name EnemyData
+extends RefCounted
+
+var is_boss: bool
+var xp_reward: int

686bf9d0c75201f5cad20e2abb62c3c7c1d9a3d3
LukasDerBaum <137618724+LukasDerBaum42@users.noreply.github.com>
2026-08-20 15:12:12 +0200

    Some Unrelated changes

--- addons/beehave_additions/start_timeline.gd
+++ addons/beehave_additions/start_timeline.gd
@@ -2,16 +2,22 @@ class_name StartTimeline
 extends ActionLeaf
 
 @export var timeline: DialogicTimeline
+@export var key: String
 
 var has_started_timeline := false
+var key_set := false
 
 func  tick(actor: Node, blackboard: Blackboard) -> int:
-	if Dialogic.current_timeline == null:
+	if GameState.has_key(key):
+		key_set = GameState.get_key(key) 
+	else:
+		key_set = false
+	if Dialogic.current_timeline == null and !key_set :
 		if has_started_timeline:
 			has_started_timeline = false
 			return SUCCESS
 		else :
-			GameState.add_key("lol", "baum")
+			GameState.add_key(key, true)
 			print(GameState.get_keys())
 			Dialogic.start(timeline)
 			has_started_timeline = true

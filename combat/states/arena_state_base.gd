4c86bb38a9bab29e778f417bf7b41291606937f6
komikronsgit <cusworthkalvin@gmail.com>
2026-09-09 08:21:13 -0400

    feat: comabt menues

--- combat/states/arena_state_base.gd
+++ combat/states/arena_state_base.gd
@@ -2,9 +2,6 @@ class_name ArenaStateBase
 extends Node
 
 @export var next_state: ArenaStateBase
-@export var first_substate: ArenaSubstateBase
-
-var current_substate: ArenaSubstateBase
 
 @onready var arena: Arena = get_parent()
 
@@ -13,15 +10,3 @@ func enter() -> void:
 
 func exit() -> void:
 	pass
-
-func change_substate(new_substate: ArenaSubstateBase, i: int):
-	if current_substate:
-		current_substate.exit()
-	current_substate = new_substate
-	current_substate.enter(i)
-
-func loop_substates(i: int):
-	if current_substate:
-		current_substate.exit()
-	current_substate = first_substate
-	current_substate.enter(i + 1)

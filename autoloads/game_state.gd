54155b2baf941c94c2503a6adfced2c8b809010d
LukasDerBaum <137618724+LukasDerBaum42@users.noreply.github.com>
2026-09-11 19:17:53 +0200

    Added some comments and fixed the one file

--- autoloads/game_state.gd
+++ autoloads/game_state.gd
@@ -11,12 +11,17 @@ var _transition_root: Node
 var _state_manager: Node
 var state_stack: Array[Node] = []
 var current_state : Node
+var poped_result: Variant
+
 
 func init(state_manager: Node, transition_root: Node) -> void:
 	_state_manager = state_manager
 	_transition_root = transition_root
 
+
 func push(state_path: String, args = null, render_underlying := false, transition_path := "res://transitions/fade_transition.tscn"):
+	
+	# transition
 	var transition: BaseTransition = null
 	if not transition_path.is_empty():
 		var transition_scene := load(transition_path) as PackedScene
@@ -25,6 +30,8 @@ func push(state_path: String, args = null, render_underlying := false, transitio
 		@warning_ignore("redundant_await")
 		await transition.play_in()
 
+	# Hide the top state if not rendering underlaying
+	# also make sure the top state is not processed or input is not processed
 	if not state_stack.is_empty():
 		var top = state_stack.back()
 		top.set_process(false)
@@ -32,29 +39,45 @@ func push(state_path: String, args = null, render_underlying := false, transitio
 		if not render_underlying:
 			top.hide()
 	
+	# Instantiate the new state and add it to the stack
 	var scene := load(state_path)
 	current_state = scene.instantiate()
 	state_stack.push_back(current_state)
 	_state_manager.add_child(current_state)
 	current_state.enter(args)
+	
+	# Fan out the transition if one was provided
 	if transition:
 		@warning_ignore("redundant_await")
 		await transition.play_out()
 		transition.queue_free()
 
-func pop(result = null):
+func pop(result = null, transition_path := "res://transitions/fade_transition.tscn"):
+	# Fan in the transition if one was provided
+	var transition: BaseTransition = null
+	if not transition_path.is_empty():
+		var transition_scene := load(transition_path) as PackedScene
+		transition = transition_scene.instantiate()
+		_transition_root.add_child(transition)
+		@warning_ignore("redundant_await")
+		await transition.play_in()
+	
+	# Pop the state from the stack and remove it from the manager
 	state_stack.pop_back()
 	_state_manager.remove_child(current_state)
 	current_state.queue_free()
+	
+	# Restore the previous state
 	if not state_stack.is_empty():
 		var previous_state = state_stack.back()
-		previous_state.show()
 		previous_state.set_process(true)
 		previous_state.set_process_input(true)
+		previous_state.show()
 		current_state = previous_state
-	return result
-
-func _ready() -> void:
-	print("current stack:", state_stack)
-# func _process(delta) -> void:
-# 	print("current stack:", state_stack)
+		
+	# Fan in the transition if one was provided
+	if transition:
+		@warning_ignore("redundant_await")
+		await transition.play_out()
+		transition.queue_free()
+	poped_result = result
--- main.gd
+++ main.gd
@@ -3,9 +3,9 @@ extends Node
 func _ready() -> void:
 	GameState.init(%StateManager, %TransitionRoot)
 	GameState.push("res://ui/ui_scenes/starting_screen.tscn")
-	PlayerManager.init(%PlayerRoot)
-	LevelManager.init(%LevelRoot, %TransitionRoot)
-	print("Main Initlized:", %PlayerRoot, %LevelRoot, %TransitionRoot)
+	# PlayerManager.init(%PlayerRoot)
+	# LevelManager.init(%LevelRoot, %TransitionRoot)
+	# print("Main Initlized:", %PlayerRoot, %LevelRoot, %TransitionRoot)
 	# LevelManager.init(%LevelRoot, %TransitionRoot)
 	
 	# PlayerManager.spawn_player()
